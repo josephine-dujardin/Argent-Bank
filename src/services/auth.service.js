ca90519c47a221d077c9720222703e55d69bb4a8
DUJARDIN Joséphine <josephine83230@gmail.com>
2023-06-23 21:58:15 +0200

    gh-pages updated

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -57,5 +57,6 @@ export const argentBankApi = createApi({
   },
 });
 
+// Export the generated hooks for each endpoint mutation
 export const { useLoginMutation, useSignUpMutation, useProfileMutation, useUpdateProfileMutation } =
   argentBankApi;
